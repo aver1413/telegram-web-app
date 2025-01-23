f5bbb12c1c7ac3df86c1354443722560681c422f
AVER <e.aver.e@yandex.ru>
2025-01-23 13:26:33 +0300

    Update username.js

--- username.js
+++ username.js
@@ -1,15 +1,5 @@
 function getUsername() {
-    if (window.Telegram && Telegram.WebApp) {
-        const initData = Telegram.WebApp.initData;
-        const params = new URLSearchParams(initData);
-        const user = params.get('user');
-        if (user) {
-            // Парсим JSON-строку user
-            const userObj = JSON.parse(user);
-            return userObj.username;
-        }
-    }
-    return null;
+    return Telegram.WebApp.initDataUnverified.user.username;
 }
 
 // Выводим username на экран
